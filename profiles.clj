8c400fa825a5e4c72dcd1e0d69fa0b68fccdb944
Shearerbeard <shearerbeard@gmail.com>
2016-10-01 08:40:46 -0600

    init commit

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                  [lein-ancient "0.6.10"]
+                  [refactor-nrepl "2.2.0"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
